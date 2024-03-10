e90cc9caec203226aabfc504e93d529ebb21618b
Vince Skahan <vinceskahan@gmail.com>
2024-03-10 15:13:45 -0700

    ts=2 all tabs

--- quickconfig.bash
+++ quickconfig.bash
@@ -15,70 +15,70 @@ ARCH_PRESENT=`which arch 2>/dev/null`
 #       incorrectly yet dpkg knows what is really running
 if [ "x${ARCH_PRESENT}" != "x" ]
 then
-        ARCH=`arch`
+  ARCH=`arch`
 fi
 
 # we will assume os-release is present rather than
 # rely on lsb_release which we know is not always present
 if [ -f /etc/os-release ]
 then
-        source /etc/os-release
+  source /etc/os-release
 fi
 
 if [ "x${DPKG_PRESENT}" != "x" ]
 then
-        #------- debian systems -------------
-        VERSION=`cat /etc/debian_version`
-        ARCH=`dpkg --print-architecture`
+  #------- debian systems -------------
+  VERSION=`cat /etc/debian_version`
+  ARCH=`dpkg --print-architecture`
 
-        # debianish
-        INSTALLED_WEEWX_PKG=`dpkg -l | grep weewx | awk '{print $3}'`
-        if [ "x${INSTALLED_WEEWX_PKG}" = "x" ]
-        then
-                INSTALLED_WEEWX_PKG="no_pkg_installed"
-        fi
+  # debianish
+  INSTALLED_WEEWX_PKG=`dpkg -l | grep weewx | awk '{print $3}'`
+  if [ "x${INSTALLED_WEEWX_PKG}" = "x" ]
+  then
+    INSTALLED_WEEWX_PKG="no_pkg_installed"
+  fi
 
-        # supersede the 'arch' command because on a pi it reports
-        # the wrong thing, but dpkg knows reality
-        ARCH=`dpkg --print-architecture`
+  # supersede the 'arch' command because on a pi it reports
+  # the wrong thing, but dpkg knows reality
+  ARCH=`dpkg --print-architecture`
 
 else
-        #------- redhat systems -------------
-        INSTALLED_WEEWX_PKG=`rpm -q weewx`
-        if [ "x${INSTALLED_WEEWX_PKG}" = "x" ]
-        then
-                INSTALLED_WEEWX_PKG="no_pkg_installed"
-        fi
+  #------- redhat systems -------------
+  INSTALLED_WEEWX_PKG=`rpm -q weewx`
+  if [ "x${INSTALLED_WEEWX_PKG}" = "x" ]
+  then
+    INSTALLED_WEEWX_PKG="no_pkg_installed"
+  fi
 fi
 
-        RUNNING_WEEWX_PROCESSES=`ps -eo cmd | grep weewxd | grep -v grep`
-        if [ "x${RUNNING_WEEWX_PROCESSES}" = "x" ]
-        then
-                RUNNING_WEEWX_PROCESSES="     none"
-        fi
+RUNNING_WEEWX_PROCESSES=`ps -eo cmd | grep weewxd | grep -v grep`
+if [ "x${RUNNING_WEEWX_PROCESSES}" = "x" ]
+then
+  RUNNING_WEEWX_PROCESSES="     none"
+fi
 
 #-----------------------------------------
 
 # look for weewx in a few likely places
 if [ -d /home/weewx ]
 then
-       HOME_WEEWX_EXISTS="true"
+  HOME_WEEWX_EXISTS="true"
 else
-       HOME_WEEWX_EXISTS="false"
+  HOME_WEEWX_EXISTS="false"
 fi
 
 if [ -d /home/pi/weewx-venv ]
 then
-        HOME_PI_VENV_EXISTS="true"
+  HOME_PI_VENV_EXISTS="true"
 else
-        HOME_PI_VENV_EXISTS="false"
+  HOME_PI_VENV_EXISTS="false"
 fi
 
 if [ -d /etc/weewx ]
 then
-        ETC_WEEWX_EXISTS="true"
+  ETC_WEEWX_EXISTS="true"
 else
-        ETC_WEEWX_EXISTS="false"
+  ETC_WEEWX_EXISTS="false"
 fi
 
 # TODO: this could even output JSON if needed
@@ -102,33 +102,33 @@ echo ""
 
 if [ ${HOME_PI_VENV_EXISTS} ]
 then
-	echo "installed weewx pip version:"
+  echo "installed weewx pip version:"
 
-	# this attempts to grab the version from the code
-	#
-	# this is a little ugly since there might be multiple python installations
-	# and varying weewx versions therein, so do some ugly output for those cases
-	WEEWX_INIT_FILES=`find /home/pi/weewx-venv/lib/python*/site-packages/weewx/__init__.py -type f -print`
-	WEEWX_INIT_FILES_COUNT=`find /home/pi/weewx-venv/lib/python*/site-packages/weewx/__init__.py -type f -print | wc -l`
-	#WEEWX_INIT_FILES_COUNT=`echo ${WEEWX_INIT_FILES} | wc -l`
-	if [ "x${WEEWX_INIT_FILES_COUNT}" != "x1" ]
-	then
-		for f in ${WEEWX_INIT_FILES}
-		do
-      echo "     in file ${f}"
-      v=`grep ^__version__ ${f} | awk '{print $3}' | sed -e s/\"//g`
-      echo "             ${v}"
-		done
-	else
-		for f in ${WEEWX_INIT_FILES}
-		do
-			# the typical one-python-version-installed is much cleaner
-		  v=`grep ^__version__ ${f} | awk '{print $3}' | sed -e s/\"//g`
-      echo "     version   = ${v}"
-		done
-	fi
+  # this attempts to grab the version from the code
+  #
+  # this is a little ugly since there might be multiple python installations
+  # and varying weewx versions therein, so do some ugly output for those cases
+  WEEWX_INIT_FILES=`find /home/pi/weewx-venv/lib/python*/site-packages/weewx/__init__.py -type f -print`
+  WEEWX_INIT_FILES_COUNT=`find /home/pi/weewx-venv/lib/python*/site-packages/weewx/__init__.py -type f -print | wc -l`
+  #WEEWX_INIT_FILES_COUNT=`echo ${WEEWX_INIT_FILES} | wc -l`
+  if [ "x${WEEWX_INIT_FILES_COUNT}" != "x1" ]
+  then
+   for f in ${WEEWX_INIT_FILES}
+   do
+    echo "     in file ${f}"
+    v=`grep ^__version__ ${f} | awk '{print $3}' | sed -e s/\"//g`
+    echo "             ${v}"
+  done
+  else
+  for f in ${WEEWX_INIT_FILES}
+  do
+    # the typical one-python-version-installed is much cleaner
+    v=`grep ^__version__ ${f} | awk '{print $3}' | sed -e s/\"//g`
+    echo "     version   = ${v}"
+  done
+fi
 else
-        HOME_PI_VENV_EXISTS="false"
+  HOME_PI_VENV_EXISTS="false"
 fi
 
 echo ""
